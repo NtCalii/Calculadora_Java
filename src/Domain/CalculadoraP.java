857e83bf5e8489eb05fa15112f3a29559d8552bd
Nathan Caliani <nathan.caliani09@gmail.com>
2024-12-24 14:21:44 -0300

    add CalculadoraP
    Adicionado pacote da calculadora que irei fazer no terminal.

---
+++ src/Domain/CalculadoraP.java
@@ -0,0 +1,8 @@
+package Domain;
+
+public class CalculadoraP {
+    public static void main(String[] args) {
+        System.out.println("Chora mas não rosna, aqui meus pit são tudo Pitbull.");
+        System.out.println("Já perdeu no argumento paizão.");
+    }
+}
